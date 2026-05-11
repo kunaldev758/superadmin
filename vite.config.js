9a40fea4cc4725b1059e4179e5299077bc0e1cd9
KhailendraActivantDeveloper <khailendraprasad.deskmoz@gmail.com>
2026-05-11 13:40:18 +0530

    remove react click to component

--- vite.config.js
+++ vite.config.js
@@ -27,8 +27,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 
-import { reactClickToComponent } from "vite-plugin-react-click-to-component";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -36,7 +34,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    reactClickToComponent(),
   ],
 
   resolve: {
